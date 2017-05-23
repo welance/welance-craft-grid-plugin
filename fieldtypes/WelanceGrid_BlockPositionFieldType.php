33f2c335dc49db350330106b8bc7f926b97c6b5d
Enrico Icardi <enrico.icardi@gmail.com>
2017-05-23 17:56:36 +0200

    :hushed: we have a working version! ~ correctly saves to DB ~ maintain values and reset DOM elements to previously saved state ~ cleaned up comments for easier understanding

--- fieldtypes/WelanceGrid_BlockPositionFieldType.php
+++ fieldtypes/WelanceGrid_BlockPositionFieldType.php
@@ -19,17 +19,20 @@ class WelanceGrid_BlockPositionFieldType extends BaseFieldType
         return AttributeType::Mixed;
     }
 
+    /**
+	 * @return value
+	 */
     public function prepValue($value)
     {
-        // Modify $value here...
-        // var_dump($value);exit;
         return $value;
     }
 
+    /**
+	 * @return value
+	 */
     public function prepValueFromPost($value)
     {
-        // Modify $value here...
-        //print_r(json_decode($value, true));
+        // serialise before storing it on DB
         return serialize($value);
     }
 
@@ -38,24 +41,32 @@ class WelanceGrid_BlockPositionFieldType extends BaseFieldType
 	 */
     public function getInputHtml($name, $value)
     {
-
+        // include assets (present in plugin_name/resources)
         craft()->templates->includeJsResource('welancegrid/js/main.js');
         craft()->templates->includeJsResource('welancegrid/js/nouislider.min.js');
 		craft()->templates->includeCssResource('welancegrid/css/main.css');
 
 		$breakpointOptions = $this->getSettings()->breakpointOptions;
-        $missingBreakpoints = array_diff($this->_getBreakpoints(), $breakpointOptions);
+        // flatten options array to easily _diff the 2 arrays
+        foreach ($this->_getBreakpoints() as $arrkey => $arrval) {
+            $allBreakpoints[] = $arrkey;
+        }
+        $missingBreakpoints = array_diff($allBreakpoints, $breakpointOptions);
 
         return craft()->templates->render('welancegrid/input', array(
             'name'  => $name,
-            'value'  => unserialize($value),
+            'value'  => unserialize($value), // unserialise while passing it to the view (twig template)
             'breakpointOptions' => $breakpointOptions,
             'allBreakpoints' => $this->_getBreakpoints(),
             'missingBreakpoints' => $missingBreakpoints
         ));
     }
 
-    // Settings
+
+    //  ----------------------------------------------------------------------------------
+    //  ----------------------------------------------------------------------- Settings -
+    //  ----------------------------------------------------------------------------------
+
     /**
 	 * @return array of settings with default value
 	 */
@@ -86,8 +97,10 @@ class WelanceGrid_BlockPositionFieldType extends BaseFieldType
 		return $settings;
     }
 
-	// Private Methods
-	// =========================================================================
+    //  ----------------------------------------------------------------------------------
+    //  ------------------------------------------------------ Private Methods / Helpers -
+    //  ----------------------------------------------------------------------------------
+
 
 	/**
 	 * Returns the position options.
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,25 +1,26 @@
 $(function() {
-    //layout mode flag (to toggle "layout mode")
+    // layout mode flag (to toggle "layout mode")
     var layoutMode = false;
-    //array to contain all slider objs
+    // array to contain all slider objs
     var allSliders = [];
-    //set main (global) OBJ for position
+    // set main (global) OBJ for position
     var pos;
-    //set OBJ for position from DB
+    // set OBJ for position from DB
     var posFromDB = null;
-    //set default OBJ for position
+    // set default OBJ for position
     var defaultPos =   {
-                            "mobile": {"offset"     : null, "width" : null},
-                            "desktop": {"offset"    : null, "width" : null},
-                            "tablet": {"offset"     : null, "width" : null},
-                            "wide": {"offset"       : null, "width" : null},
+                            "mobile":       {"offset"    : 0, "width" : 12},
+                            "desktop":      {"offset"    : 0, "width" : 12},
+                            "tablet":       {"offset"    : 0, "width" : 12},
+                            "wide":         {"offset"    : 0, "width" : 12},
                             "new_row"               : false,
                             "full_height"           : false,
                             "full_width"            : false
                         };
 
-
-    //layout mode toggle
+    // layout mode toggle
+    // this is to view all the sliders closer to each other
+    // and better play with the layout of the webpage
     $('.wg-layout-mode').on('click', function(ev){
         if(layoutMode === false){
             $('.fields .field').siblings().not('*[id*=colWidth]').hide();
@@ -30,7 +31,7 @@ $(function() {
         }
     });
 
-    //open all tabs relative to the clicked tab on a "content block" from Matrix field
+    // open all tabs relative to the clicked tab on a "content block" from Matrix field
     $('*[class*=-check]').on('click', function(ev){
         if($(ev.target).hasClass('desktop-check')){
             _hideAllBreakpoints();
@@ -50,26 +51,29 @@ $(function() {
             $('.wide-content').show();
         }
     });
-    //trigger default click on "open"
+    // trigger default click on "open"
     $('.desktop-check').trigger('click');
 
+    // get all checkboxes with sel-row class
     $('.sel-row').on('change', function(ev){
-        //get Position from the right place!
+        // get Position from the right place!
         pos = _getPosFromCloseEl($(ev.target));
 
+        // "new row"" checkbox
         if($(ev.target).hasClass('row-new')){
             if($(ev.target).is(':checked')){
                 pos.new_row = true;
             }else{
                 pos.new_row = false;
             }
+        // "full width"" checkbox
         }else if($(ev.target).hasClass('row-full-width')){
             if($(ev.target).is(':checked')){
                 pos.full_width = true;
             }else{
                 pos.full_width = false;
             }
-
+        // "full height"" checkbox
         }else if($(ev.target).hasClass('row-full-height')){
             if($(ev.target).is(':checked')){
                 pos.full_height = true;
@@ -78,13 +82,33 @@ $(function() {
             }
         }
 
-        //write obj inside "closest" textarea (textarea of current position block)
+        if(pos.full_height){
+            $(ev.target).closest('.welance-grid').find('.noUi-connect').addClass('noUi-connect--hi');
+        }else{
+            $(ev.target).closest('.welance-grid').find('.noUi-connect').removeClass('noUi-connect--hi');
+        }
+        if(pos.full_width){
+            $(ev.target).closest('.welance-grid').find('.wrap-slider').addClass('wrap-slider--full');
+            $(ev.target).closest('.welance-grid').find('.slider').addClass('slider--full');
+        }else{
+            $(ev.target).closest('.welance-grid').find('.wrap-slider').removeClass('wrap-slider--full');
+            $(ev.target).closest('.welance-grid').find('.slider').removeClass('slider--full');
+        }
+        if(pos.new_row){
+            $(ev.target).closest('.welance-grid').find('.noUi-connect').addClass('noUi-connect--new');
+        }else{
+            $(ev.target).closest('.welance-grid').find('.noUi-connect').removeClass('noUi-connect--new');
+        }
+
+
+        // write obj inside "closest" textarea (textarea of current position block)
         $(ev.target).closest('.welance-grid').find('.wg-data').val(JSON.stringify(pos));
     });
 
-    //go through all the sliders of the (admin) page and set a noUIslider.js instance
+    // go through all the sliders of the (admin) page and set a noUIslider.js instance
     var sliders = $('.slider');
     for ( var i = 0; i < sliders.length; i++ ) {
+        // push every slider inside array to then have a reference to them
         allSliders.push(noUiSlider.create($(sliders[i])[0], {
             start: [ $(sliders[i]).data('from'), $(sliders[i]).data('to') ],
             step: 1,
@@ -100,41 +124,134 @@ $(function() {
             }
         }));
 
-        //get latest added slider (current)
+        // get latest added slider (current)
         var thisSlider = allSliders[allSliders.length-1];
-        //assign to current slider the action to change the OBJ "pos" (position)
+        // assign to current slider the action to change the OBJ "pos" (position)
         thisSlider.on('change', function(val){
+            // set new values and change DOM accordingly
             setObjVals(val, $(this));
         });
+        // get values
+        var initPos = getInitVals($(thisSlider));
+        // and set the slider handles accordingly
+        thisSlider.set([initPos.offset,initPos.width]);
+
     }
 
+    // set new values and change DOM accordingly
     function setObjVals(val, slider){
-        //get target and create a jquery element out of it (to quickly get properties and helpers w/ jquery)
+        // get target and create a jquery element out of it (to quickly get properties and helpers w/ jquery)
         var $sliderEl = $(slider[0].target);
-        //get Position from the right place!
+        // get Position from the right place!
         pos = _getPosFromCloseEl($sliderEl);
 
         try{
-            //find out which breakpoint is related to the slider and set values
+            // find out which breakpoint is related to the slider and set values
             if($sliderEl.hasClass('desktop')){
+                // set changes to global var "pos" (the global source of truth)
                 pos.desktop.offset = val[0];
                 pos.desktop.width = val[1];
+                // set the small numbers next to the right tab with the new value
+                $sliderEl.closest('.welance-grid').find('.wg-desktop-offset').html(pos.desktop.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-desktop-width').html(pos.desktop.width);
             }else if($sliderEl.hasClass('mobile')){
+                // set changes to global var "pos" (the global source of truth)
                 pos.mobile.offset = val[0];
                 pos.mobile.width = val[1];
+                // set the small numbers next to the right tab with the new value
+                $sliderEl.closest('.welance-grid').find('.wg-mobile-offset').html(pos.mobile.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-mobile-width').html(pos.mobile.width);
             }else if($sliderEl.hasClass('tablet')){
+                // set changes to global var "pos" (the global source of truth)
                 pos.tablet.offset = val[0];
                 pos.tablet.width = val[1];
+                // set the small numbers next to the right tab with the new value
+                $sliderEl.closest('.welance-grid').find('.wg-tablet-offset').html(pos.tablet.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-tablet-width').html(pos.tablet.width);
             }else if($sliderEl.hasClass('wide')){
+                // set changes to global var "pos" (the global source of truth)
                 pos.wide.offset = val[0];
                 pos.wide.width = val[1];
+                // set the small numbers next to the right tab with the new value
+                $sliderEl.closest('.welance-grid').find('.wg-wide-offset').html(pos.wide.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-wide-width').html(pos.wide.width);
             }
+
+            // write obj inside "closest" textarea (textarea of current position block)
+            $sliderEl.closest('.welance-grid').find('.wg-data').val(JSON.stringify(pos));
+
         }catch(err){
             console.error("Something went wrong modifying main JSON position:", pos);
         }
+    }
 
-        //write obj inside "closest" textarea (textarea of current position block)
-        $sliderEl.closest('.welance-grid').find('.wg-data').val(JSON.stringify(pos));
+    function getInitVals(slider){
+        // get target and create a jquery element out of it (to quickly get properties and helpers w/ jquery)
+        var $sliderEl = $(slider[0].target);
+        // get Position from the right place!
+        pos = _getPosFromCloseEl($sliderEl);
+        var data = {"offset": null, "width": null};
+
+        try{
+            // find out which breakpoint is related to the slider and set values
+            if($sliderEl.hasClass('desktop')){
+                // define data to return, to set the correct slider in the right position
+                data.offset = pos.desktop.offset;
+                data.width = pos.desktop.width;
+                // set the small numbers next to the right tab with the new value coming from pos (source of truth)
+                $sliderEl.closest('.welance-grid').find('.wg-desktop-offset').html(pos.desktop.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-desktop-width').html(pos.desktop.width);
+            }else if($sliderEl.hasClass('mobile')){
+                // define data to return, to set the correct slider in the right position
+                data.offset = pos.mobile.offset;
+                data.width = pos.mobile.width;
+                // set the small numbers next to the right tab with the new value coming from pos (source of truth)
+                $sliderEl.closest('.welance-grid').find('.wg-mobile-offset').html(pos.mobile.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-mobile-width').html(pos.mobile.width);
+            }else if($sliderEl.hasClass('tablet')){
+                // define data to return, to set the correct slider in the right position
+                data.offset = pos.tablet.offset;
+                data.width = pos.tablet.width;
+                // set the small numbers next to the right tab with the new value coming from pos (source of truth)
+                $sliderEl.closest('.welance-grid').find('.wg-tablet-offset').html(pos.tablet.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-tablet-width').html(pos.tablet.width);
+            }else if($sliderEl.hasClass('wide')){
+                // define data to return, to set the correct slider in the right position
+                data.offset = pos.wide.offset;
+                data.width = pos.wide.width;
+                // set the small numbers next to the right tab with the new value coming from pos (source of truth)
+                $sliderEl.closest('.welance-grid').find('.wg-wide-offset').html(pos.wide.offset);
+                $sliderEl.closest('.welance-grid').find('.wg-wide-width').html(pos.wide.width);
+            }
+            // set "global" (to context "block") checkboxes for row: width, height, new
+            $sliderEl.closest('.welance-grid').find('.row-new').prop('checked', pos.new_row);
+            $sliderEl.closest('.welance-grid').find('.row-full-width').prop('checked', pos.full_width);
+            $sliderEl.closest('.welance-grid').find('.row-full-height').prop('checked', pos.full_height);
+
+            if(pos.full_height){
+                $sliderEl.closest('.welance-grid').find('.noUi-connect').addClass('noUi-connect--hi');
+            }else{
+                $sliderEl.closest('.welance-grid').find('.noUi-connect').removeClass('noUi-connect--hi');
+            }
+            if(pos.full_width){
+                $sliderEl.closest('.welance-grid').find('.wrap-slider').addClass('wrap-slider--full');
+                $sliderEl.closest('.welance-grid').find('.slider').addClass('slider--full');
+            }else{
+                $sliderEl.closest('.welance-grid').find('.wrap-slider').removeClass('wrap-slider--full');
+                $sliderEl.closest('.welance-grid').find('.slider').removeClass('slider--full');
+            }
+            if(pos.new_row){
+                $sliderEl.closest('.welance-grid').find('.noUi-connect').addClass('noUi-connect--new');
+            }else{
+                $sliderEl.closest('.welance-grid').find('.noUi-connect').removeClass('noUi-connect--new');
+            }
+
+            // return the data offset and width (flattened) to the right slider
+            return data;
+
+        }catch(err){
+            console.error("Something went wrong giving initial values to sliders:", data);
+        }
     }
 
     function _hideAllBreakpoints(){
@@ -142,9 +259,9 @@ $(function() {
         $('.desktop-content, .mobile-content, .tablet-content, .wide-content').hide();
     }
 
-    //this function requires closeElement which is an element inside the right welance-grid context (right block)
+    // this function requires closeElement which is an element inside the right welance-grid context (right block)
     function _getPosFromCloseEl($closeElement){
-        //globalposFromDB
+        // globalposFromDB
         if($closeElement.closest('.welance-grid').find('.wg-data').val()){
             posFromDB = JSON.parse($closeElement.closest('.welance-grid').find('.wg-data').val());
         }
